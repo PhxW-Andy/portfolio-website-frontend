874d790d3790e01987964479d18aa7a792ecdc17
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-08-20 22:36:17 +0200

    changes contact submit function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,8 +32,17 @@ const Contact = () => {
   );
 
   const onSubmit = async (formData) => {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json, text/plain, */*",
+      },
+    };
+
     try {
-      await axios.post(URL, { formData }).then((res) => {
+      await axios.post(URL, formData, options).then((res) => {
+        console.log(res.data);
         if (res.data == "success") {
           setConfirm(true), reset();
         }
